87790a8e4454943a7bfac8a9a45046e42fceeaaf
Alfonso <alfonso.ramat@gmail.com>
2020-12-13 22:49:24 -0300

    Mejorada la captura de errores

--- handlers/ErrorHandlers.js
+++ handlers/ErrorHandlers.js
@@ -1,29 +1,46 @@
 const createError = require("http-errors");
 const logger = require("../helpers/createLogger");
 const { v4 } = require("uuid");
+const { create } = require("../modelos/usuarios.model");
 
 async function defaultRoute(req, res, next) {
   next(createError.NotFound("Direccion no encontrada"));
 }
 
 async function errorHandler(err, req, res, next) {
+  // Genera un numero aleatorio para identificar el error en los logs
   const randomErrorId = v4();
-
-  const mensaje = err.message || "Hola tito";
   await logger.log({
-      level: 'error',
-      errorID: randomErrorId,
-      message: mensaje,
-      statusCode: err.status,
+    level: "error",
+    errorID: randomErrorId,
+    message: err.message,
+    body: req.body,
+    statusCode: err.status,
   });
-
-  res.status(err.status || 500).json({
-    error: {
-      codigo: randomErrorId,
-      message:
-        "Hubo un problema, por favor remita el codigo a los desarrolladores para su analisis",
-    },
-  });
+// Si el error proviene de Joi significa que la informacion enviada en la peticion es incorrecta o incompleta
+  if (err.isJoi === true) {
+    return res.status(400).json({
+      error: {
+        codigo: randomErrorId,
+        message: "La informacion proporcionada es incorrecta o esta incompleta",
+      },
+    });
+  } else if (err instanceof createError.HttpError) { // Si el error es de tipo httpError, es un tipo de error esperado y el mensaje puede ser mostrado a los usuarios
+    res.status(err.status || 500).json({
+      error: {
+        codigo: randomErrorId,
+        message: err.message,
+      },
+    });
+  } else { // Si el error llega a esta instancia, es un error inesperado en el servidor y no debe ser revelado al usuario, se le envia el codigo y un mensaje para remitirlo a nosotros
+    res.status(err.status || 500).json({ 
+      error: {
+        codigo: randomErrorId,
+        message:
+          "Hubo un problema, por favor remita el codigo a los desarrolladores para su analisis",
+      },
+    });
+  }
 }
 
 module.exports = { defaultRoute, errorHandler };
