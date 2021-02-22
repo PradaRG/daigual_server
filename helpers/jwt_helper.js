313c308cc972e109422582df026a715085ac1677
Alfonso Ramat <alfonso.ramat@gmail.com>
2021-02-22 01:07:21 -0300

    Cookies are being sent =)

--- helpers/jwt_helper.js
+++ helpers/jwt_helper.js
@@ -2,17 +2,18 @@ const JWT = require("jsonwebtoken");
 const createError = require("http-errors");
 const cliente = require("../helpers/init_redis");
 
+
 module.exports = {
   signAccessToken: (userId) => {
     return new Promise((resolve, reject) => {
       const payload = {};
       const secret = process.env.ACCESS_TOKEN_SECRET;
       const options = {
-        expiresIn: "10s",
+        expiresIn: "5m",
         issuer: "rtdevelopment.com.ar",
         audience: userId,
       };
-      
+
       JWT.sign(payload, secret, options, (err, token) => {
         if (err) return reject(err);
         return resolve(token);
@@ -21,13 +22,14 @@ module.exports = {
   },
   verifyAccessToken: (req, res, next) => {
     if (!req.headers["authorization"]) return next(createError.Unauthorized());
+    
     const authHeader = req.headers["authorization"];
     const bearerToken = authHeader.split(" ");
     const token = bearerToken[1];
     JWT.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, payload) => {
       if (err) return next(createError.Unauthorized());
-      req.payload(payload);
-      next();
+      req.payload(payload);  
+      next();  
     });
   },
   signRefreshToken: (userId) => {
@@ -35,7 +37,7 @@ module.exports = {
       const payload = {};
       const secret = process.env.REFRESH_TOKEN_SECRET;
       const options = {
-        expiresIn: "1y",
+        expiresIn: "30 days",
         issuer: "rtdevelopment.com.ar",
         audience: userId,
       };
@@ -43,7 +45,7 @@ module.exports = {
       JWT.sign(payload, secret, options, (err, token) => {
         if (err) return reject(err);
         // Almacena el token en Redis
-        cliente.SET(userId, token, 'EX', 365 * 24 * 60 * 60, (err, respuesta) => {
+        cliente.SET(userId, token, 'EX', 30 * 24 * 60 * 60, (err, respuesta) => {
           if (err) return (createError.InternalServerError());
           return resolve(token);
         }
@@ -65,5 +67,5 @@ module.exports = {
       }
       );
     });
-  },
+  }
 };
