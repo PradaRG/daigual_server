8d97666531f04875e211d733f40bc14140850f43
Alfonso Ramat <alfonso.ramat@gmail.com>
2021-03-04 23:41:42 -0300

    Proveedor update pendiente de pruebas

--- handlers/Proveedor.route.js
+++ handlers/Proveedor.route.js
@@ -1,40 +1,64 @@
 const express = require('express');
+const createError = require('http-errors');
+const { create } = require('../modelos/proveedor.model');
 const router = express.Router();
 const Proveedor = require('../modelos/proveedor.model');
 
 const proveedores = [{
     "codigoInterno": "es",
-     "nombre": "El sembrador SRL",
-     "email": "sembrame@esta.com",
-     "telefono": "343-1234565",
-     "descripcion": "Vendemos alcohol para emborrachar a tu suegra" 
- },
- {
+    "nombre": "El sembrador SRL",
+    "email": "sembrame@esta.com",
+    "telefono": "343-1234565",
+    "descripcion": "Vendemos alcohol para emborrachar a tu suegra"
+},
+{
     "codigoInterno": "pc",
-     "nombre": "Pollos calisa",
-     "email": "tupolla@sabrosa.net",
-     "telefono": "343-1132485",
-     "descripcion": "A las chicas de verdad les gusta el pollo frito" 
- }, {
+    "nombre": "Pollos calisa",
+    "email": "tupolla@sabrosa.net",
+    "telefono": "343-1132485",
+    "descripcion": "A las chicas de verdad les gusta el pollo frito"
+}, {
     "codigoInterno": "cm",
-     "nombre": "Chango mas",
-     "email": "changuito@chichon.com",
-     "telefono": "343-121234765",
-     "descripcion": "Precios bajos todos los dias" 
- }];
+    "nombre": "Chango mas",
+    "email": "changuito@chichon.com",
+    "telefono": "343-121234765",
+    "descripcion": "Precios bajos todos los dias"
+}];
 
 
-router.post('/create', async(req, res) =>{
+router.post('/create', async (req, res, next) => {
 
-    const {codigoInterno, nombre, email, telefono, descripcion} = req.body;
+    const { codigoInterno, nombre, email, telefono, descripcion } = req.body;
     //TODO: Requiere validacion de datos
-    const createdProvider = await Proveedor.create({codigoInterno, nombre, email, telefono, descripcion});
+    const createdProvider = await Proveedor.create({ codigoInterno, nombre, email, telefono, descripcion });
     res.status(201).json(createdProvider);
 });
 
-router.get('/bulk', async (req, res)=> {
-const result =await Proveedor.bulkCreate(proveedores);
-res.sendStatus(200);
+router.post('/update', async (req, res, next) => {
+try {
+    
+    const { id, codigoInterno, nombre, email, telefono, descripcion } = req.body;
+
+    const prov = await Proveedor.findByPk(id);
+
+    if (!prov) throw createError.NotFound('Proveedor no encontrado');
+
+    const updatedUser = await prov.update({
+        codigoInterno,
+        nombre,
+        email,
+        telefono,
+        descripcion
+    });
+res.status(200).json(updatedUser);
+} catch (error) {
+   next(error);
+}
+});
+
+router.get('/bulk', async (req, res) => {
+    const result = await Proveedor.bulkCreate(proveedores);
+    res.sendStatus(200);
 });
 
 router.get('/getAll', async (req, res) => {
