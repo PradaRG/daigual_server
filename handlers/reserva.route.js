e76c0ef6a7999b9fd32da4586973df17cf73b2a0
Alfonso Ramat <alfonso.ramat@gmail.com>
2021-07-20 01:53:05 -0300

    Reserva

--- handlers/reserva.route.js
+++ handlers/reserva.route.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const createError = require('http-errors');
+const Cliente = require('../modelos/cliente.model');
+const Movimientos = require('../modelos/movimientos.model');
+const Producto = require('../modelos/productos.model');
 const Reserva = require('../modelos/reserva.model');
+const Usuario = require('../modelos/usuarios.model');
 const router = express.Router();
 
 router.get('/', async (req, res, next) => {
@@ -17,6 +21,24 @@ router.post('/', async (req, res, next) => {
     try {
         const reservaCreada = await Reserva.create({ ...req.body });
         if (!reservaCreada) throw createError.InternalServerError("No se pudo crear la reserva");
+        const cliente = await Cliente.findByPk(reservaCreada.ClienteId);
+        const producto = await Producto.findByPk(reservaCreada.ProductoId);
+
+        const user = await Usuario.findOne({
+            where: {
+                ventaRapida: req.body.ventaRapida
+            }
+        });
+
+        await Movimientos.create({
+            descripcion: `Pago del cliente ${cliente.nombre}, por la reserva del producto ${producto.nombre}`,
+            operacion: "deposito",
+            monto: reservaCreada.montoAbonado,
+            UsuarioId: user.id,
+            estado:"finalizada",
+            CajaId: req.body.CajaId,
+            ReservaId: reservaCreada.id
+        });
         res.status(200).json(reservaCreada);
     } catch (error) {
         next(error);
